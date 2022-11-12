6daf081da426d172abf5946ef37684042f241483
Alexperovo <alexperovo@gmail.com>
2022-11-12 16:56:31 +0300

    половина готова

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 ﻿// Напишете программу, которая на вход принимает два числа и выдает, какое число боольше, а какое меньше.
-Console.WriteLine("Эта программ определяет какое число больше");
+Console.WriteLine("Эта программ определяет какое из чисел больше");
 Console.Write("Введите 1 число: ");
 int num = Convert.ToInt32(Console.ReadLine ());
 
 Console.Write("Введите 2 число: ");
 int num1 = Convert.ToInt32(Console.ReadLine ());
-
+// вводные получены
+if (num > num1)
+{ 
+    Console.Write ("Большее число ");
+    Console.Write(num);
+}
 
